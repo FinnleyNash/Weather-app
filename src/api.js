ef6d546b0764eb1df7b5140a4c85f88f84c5646d
FinnleyNash <finnleynash.nash@gmail.com>
2024-07-05 10:59:38 +0100

    first

---
+++ src/api.js
@@ -0,0 +1,13 @@
+
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': '8203ac1ab7msh13d4d452e78d9c1p15b3e8jsn2e00e951df29',
+		'x-rapidapi-host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '9375ba5f866da1bb1758753ef73a52ae';
